9ec3f096144bbdd8377592752355fc94225ae017
Kyle King <KyleKing@users.noreply.github.com>
2022-10-14 09:25:09 -0400

    feat: initialize with copier

---
+++ README.md
@@ -0,0 +1,5 @@
+# game (game)
+
+Student/player implementation of the design_patterns_island game!
+
+Documentation can be found on [Github (./docs)](./docs), [PyPi](https://pypi.org/project/game/), or [Hosted](https://game.kyleking.me/)!
---
+++ game/__init__.py
@@ -0,0 +1,10 @@
+"""game."""
+
+from loguru import logger
+
+__version__ = '0.0.1'
+__pkg_name__ = 'game'
+
+logger.disable(__pkg_name__)
+
+# ====== Above is the recommended code from calcipy_template and may be updated on new releases ======
