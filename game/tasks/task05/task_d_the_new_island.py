21c6038cae93531d47b5625cf12af0de9ebb1358
Kyle King <KyleKing@users.noreply.github.com>
2022-12-04 19:40:52 -0500

    wip: start task 5

--- game/tasks/task05/task_d_the_new_island.py
+++ game/tasks/task05/task_d_the_new_island.py
@@ -8,6 +8,13 @@ The fifth task will be to apply the "D" of the S.O.L.I.D design principles to la
 
 """
 
+from game.core.models import EntityAttr, SpriteState
+from game.core.registration import Register, SpriteRegister
+from game.core.view_strategies.movement import cardinal_key_move
+from game.core.views import GameSprite
+
+SOURCE_NAME = "task_5"  # FYI: Required for code reload
+
 """
 
 This task hasn't been Implemented yet!
@@ -20,3 +27,39 @@ Alternatively, for this task, the user could create logic to generate vegetation
 static items that can be picked up or used
 
 """
+
+# Copy over the sprites from community-rpg!
+# Maybe implementation is how the character is animated? Flapping vs. walking?
+
+
+class BatFamiliar:  # ":assets:characters/Animals/pipo-nekonin020.png"
+    def fly_spiral(self):
+        pass
+
+
+class PandaFamiliar:  # pipo-nekonin018.png
+    def wander(self):
+        pass
+
+
+class Familiar:
+    def tbd(self):
+        pass
+
+
+# FYI: Required for code reload
+def load_sprites(sprite_register: SpriteRegister) -> None:
+    """Common entry point for modules that register a graphical element."""
+    resource = (
+        ":resources:images/animated_characters/female_person/femalePerson_idle.png"
+    )
+    attr = EntityAttr(step_size=999)
+    state = SpriteState(sprite_resource=resource, center_x=10, center_y=10)
+    register = Register(
+        sprite=GameSprite(attr, state),
+        source=SOURCE_NAME,
+        on_key_release=cardinal_key_move,
+        on_mouse_motion=(lambda _cls, _x, _y, _dx, _dy: None),
+        on_update=(lambda _cls, _x: None),
+    )
+    sprite_register.register_sprite(register)
