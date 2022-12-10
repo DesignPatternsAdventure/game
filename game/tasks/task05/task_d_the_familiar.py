a2206d3c545d7750f07cb4c06d7ecc8aa07b601c
Kyle King <KyleKing@users.noreply.github.com>
2022-12-10 17:58:13 -0500

    refactor: half-solve Task 5 (#44)

--- game/tasks/task05/task_d_the_familiar.py
+++ game/tasks/task05/task_d_the_familiar.py
@@ -8,8 +8,12 @@ The fifth task will be to apply the "D" of the S.O.L.I.D design principles to me
 
 """
 
+import itertools
 import random
+from collections.abc import Iterable as SupportsNext
 from datetime import datetime
+from enum import Enum
+from typing import TYPE_CHECKING
 
 import arcade.key
 from beartype import beartype
@@ -28,6 +32,10 @@ from ...core.game_clock import GameClock
 from ...core.models import EntityAttr, SpriteState
 from ...core.registration import CameraView, Register, SpriteRegister
 from ...core.views import GameSprite
+
+if TYPE_CHECKING:
+    from _typeshed import SupportsNext  # noqa: F811
+
 
 SOURCE_NAME = "task_5"  # FYI: Required for code reload
 
@@ -75,6 +83,13 @@ def get_vector_to_object(
     return tuple(vector)
 
 
+class SpriteMotion(Enum):
+
+    FOLLOW = "follow"
+    SEEK_TREASURE = "seek_treasure"
+    RANDOM = "random"
+
+
 class FamiliarSprite(GameSprite):
 
     movement_speed: NumT = 0.5
@@ -82,9 +97,15 @@ class FamiliarSprite(GameSprite):
     player_center: tuple[NumT, NumT] = (STARTING_X, STARTING_Y)
     camera_view: CameraView = CameraView(center=player_center)
 
-    # TODO: When this task is complete, `self.follow` and `self.find_chest` shouldn't be necessary
-    follow: bool = True
-    find_chest: bool = False
+    #
+    # FYI: Some solutions that utilize Dependency Injection may involve changes
+    #   to how this code works and the SpriteMotion enum. Feel free to make any
+    #   changes that you feel are appropriate and test frequently!
+    #
+    motion_algorithms: SupportsNext[SpriteMotion] = itertools.cycle(
+        [SpriteMotion.FOLLOW, SpriteMotion.SEEK_TREASURE, SpriteMotion.RANDOM]
+    )
+    active_algorithm: SpriteMotion = SpriteMotion.FOLLOW
 
     @classmethod
     @beartype
@@ -101,25 +122,43 @@ class FamiliarSprite(GameSprite):
     @beartype
     def on_update(self, game_clock: GameClock) -> None:
         center = (self.center_x, self.center_y)
-        in_view = self.camera_view.in_view(center)
         # Wait for the user to find the Familiar after reload
-        if not in_view:
+        if not self.camera_view.in_view(center):
             self.change_x, self.change_y = 0, 0
         # For more natural movement, set the Familiar's trajectory to 0 every 0.1s
         elif self.next_update is None or game_clock.current_time > self.next_update:
             self.change_x, self.change_y = 0, 0
             self.next_update = game_clock.get_time_in_future(0.1)
         # Otherwise calculate the trajectory for the Familiar
+
+        # TODO: This class currently needs to understand how each SpriteMotion is
+        #   implemented. The goal for this task is to extract this logic so that
+        #   dependency on motion algorithm is inverted. There are many possible
+        #   solutions and this code currently works as-is, so this is opportunity
+        #   to practice what you've learned so far and think about refactoring a
+        #   challenge like you might see in real code bases.
         #
-        # TODO: These three conditions are what you will be refactoring
-        #   How can this logic be extracted and specified outside of this
-        #   class without the need for attributes, like 'self.follow'?
+        # TODO: At minimum, you'll want to make this function less complex to resolve
+        #   the warning from flake8 ("R701 - on_update is too complex") that appears
+        #   if you run the "doit run check" task
         #
-        elif self.follow:
+        # As a tip, what kind of abstract interface would you need? What future
+        #   features might be possible that could change the needs for the interface?
+        #
+        # There are many possible solutions, so enjoy this final challenge!
+        #
+        elif True:
+            # Go ahead and remove this error once you're ready to start on Task 5,
+            #   this is just to help you find the right place in the code. Good luck!
+            raise NotImplementedError(
+                "Your familiar needs your help!\
+                \nEdit the code in 'task05/task_d_the_familiar.py' to help"
+            )
+        elif self.active_algorithm == SpriteMotion.FOLLOW:
             offset = 20
             destination = tuple(pos + offset for pos in self.player_center)
             self.change_x, self.change_y = get_vector_to_object(center, destination)
-        elif self.find_chest:
+        elif self.active_algorithm == SpriteMotion.SEEK_TREASURE:
             destination = (TREASURE_CHEST_X, TREASURE_CHEST_Y)
             self.change_x, self.change_y = get_vector_to_object(center, destination)
             # Check that the Familiar would still be visible
@@ -132,31 +171,13 @@ class FamiliarSprite(GameSprite):
                 self.movement_speed
             )
 
-        if in_view:
-            raise NotImplementedError(
-                "Your familiar needs your help!\
-                \nEdit the code in 'task05/task_d_the_familiar.py' to help"
-            )
-
         super().on_update(game_clock)
 
     @beartype
     def on_key_press(self, key: int, modifiers: int) -> None:
         if key == arcade.key.F:
-            # TODO: Instead of toggling these flags, this key press could call some
-            #   method on the injected dependency
-            if self.follow:
-                logger.warning("Switching to 'Find Chest' motion!")
-                self.follow = False
-                self.find_chest = True
-            elif self.find_chest:
-                logger.warning("Switching to 'Random' motion")
-                self.follow = False
-                self.find_chest = False
-            else:
-                logger.warning("Switching to 'Follow' motion")
-                self.follow = True
-                self.find_chest = False
+            self.active_algorithm = next(self.motion_algorithms)
+            logger.warning(f"Familiar is now using '{self.active_algorithm}' motion")
 
     @beartype
     def on_player_sprite_motion(self, player_center: tuple[NumT, NumT]) -> None:
